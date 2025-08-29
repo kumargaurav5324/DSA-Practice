157fd96ca596ccc98122a27b307535dbf0c38ffe
kumargaurav5324 <142592810+kumargaurav5324@users.noreply.github.com>
2025-08-29 18:38:39 +0530

    Add files via upload

---
+++ MaxSubArraySum.java
@@ -0,0 +1,32 @@
+package SlidingWindows;
+
+public class MaxSubArraySum {
+
+    public static int maxSum(int[] arr, int k){
+        int  i =0;
+        int j = 0;
+        int sum  = 0;
+        int maxSum = 0;
+
+        while(j < arr.length){
+            sum  = sum  +arr[j];
+
+            if(j-i + 1 < k ){
+                j++;
+            }
+            else if (j - i +1 == k){
+                maxSum = Math.max(sum , maxSum);
+                sum  = sum - arr[i];
+                i++;
+                j++;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,1,7,4,5,9,5, 67};
+        int k = 4;
+        System.out.println(maxSum(arr,k));
+    }
+}
